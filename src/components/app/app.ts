78525e70db41e0959eb3272d07c52c49d587bf57
Marina Fando <marinafando@gmail.com>
2023-01-30 18:39:19 +0100

    feat: migrate app component to TypeScript

---
+++ src/components/app/app.ts
@@ -0,0 +1,27 @@
+import AppController from '../controller/controller';
+import { AppView, NewsData, SourcesData } from '../view/appView';
+
+class App {
+    controller: AppController;
+    view: AppView;
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start() {
+        const sourcesElement = document.querySelector('.sources');
+        if (sourcesElement) {
+            sourcesElement.addEventListener('click', (e) =>
+                this.controller.getNews(e, (data: NewsData) => {
+                    this.view.drawNews(data);
+                })
+            );
+            this.controller.getSources((data: SourcesData) => {
+                this.view.drawSources(data);
+            });
+        }
+    }
+}
+
+export default App;
