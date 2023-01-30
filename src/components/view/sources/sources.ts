d924a93538ef630f4abf15fc0f16d54c13761821
Marina Fando <marinafando@gmail.com>
2023-01-30 07:13:19 +0100

    feat: migrate sources component to TypeScript

---
+++ src/components/view/sources/sources.ts
@@ -0,0 +1,34 @@
+import './sources.css';
+
+export interface Source {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
+
+class Sources {
+    draw(data: Source[]) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        sourceItemTemp &&
+            data.forEach((item: Source) => {
+                const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+
+                if (sourceClone) {
+                    sourceClone.querySelector('.source__item-name')!.textContent = item.name;
+                    sourceClone.querySelector('.source__item')!.setAttribute('data-source-id', item.id);
+
+                    fragment.append(sourceClone);
+                }
+            });
+
+        document.querySelector('.sources')!.append(fragment);
+    }
+}
+
+export default Sources;
