249e92594cd363ed5e14e889cd63dd17353db4ac
Marina Fando <marinafando@gmail.com>
2023-01-30 07:34:33 +0100

    feat: migrate AppLoader component to TypeScript

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '097a6704bf0c4318b71ef782ef6e555c',
         });
     }
 }
