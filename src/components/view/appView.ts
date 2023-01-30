46bd735bac2273ae51fe0eca96faa6b445c69463
Marina Fando <marinafando@gmail.com>
2023-01-30 07:14:23 +0100

    feat: migrate AppView component to TypeScript

---
+++ src/components/view/appView.ts
@@ -0,0 +1,34 @@
+import News, { NewsObject } from './news/news';
+import Sources, { Source } from './sources/sources';
+
+export interface NewsData {
+    articles: NewsObject[];
+    status: string;
+    totalResults: number;
+}
+
+interface SourcesData {
+    sources: Source[];
+    status: string;
+}
+
+export class AppView {
+    news: News;
+    sources: Sources;
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
+
+    drawNews(data: NewsData) {
+        const values = data?.articles ? data?.articles : [];
+        this.news.draw(values);
+    }
+
+    drawSources(data: SourcesData) {
+        const values = data?.sources ? data?.sources : [];
+        this.sources.draw(values);
+    }
+}
+
+export default AppView;
